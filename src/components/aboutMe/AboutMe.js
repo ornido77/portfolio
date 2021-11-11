fddac9a3b30eecf405f2546647cf9b2d98611f5a
Sonny Ornido <sonnyornido0@gmail.com>
2021-11-11 21:46:03 +0800

    fix

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -24,40 +24,25 @@ function AboutMe() {
 				</div>
 				<div className="right">
 					<div className="imgContainer">
-						<img src="" alt="" />
+						<img src="assets/portrait.jpg" alt="" />
 					</div>
 				</div>
 			</div>
 			<div className="bot">
 				<div className="imgContainer">
-					<img
-						src="https://lh3.googleusercontent.com/3TTwkd98wBpk5jEyRAijyNjhnWc6NKlIOypV1PECBTXW2xTECjjcX9N9DnfnHDVWuDAS_8NpLp8VChNjymilw4RGmgNW0z3v0xHIr_rmvzkD7RlJbNiI40I6t-rS0X3X088lOe90x-PF4bOpR5JWrha2wePwFm9B31WkAK8ZpcDnQWqWrbCXKw9WWu_TYudjiNR6W_O1oGDoeZng9iVSi3aznBqVfNyj1wg4b46NMcNbx-kfimC_c4XV1Gezl0b4iY6g3m1r0M0jM9oQU15hTmIIY1gcUeAwUd4_ZCJUF25gR4iovu6tA2pWFRfkEtNEcmNGd-zE5wJHOZNRUZlh27wOl_b8jtNTX67ccQbOX-HaO60yCHDNiQ1_2hcZ5pGhD_74EidrkvE6_w1dPgrElR-9fXGHI7yZ6xcFKNzGhQAzUQ6eZTJo3WiOkBqL9Y9N-ZhRNSwWvAvE7lEtq2bIAAur4wixWaSFWHLW53lew3KiWpZQBx6iZze4FuO5vR-cGG73rWB6sJ7Zp4xIOCOM6XU4fc-nrxzflDoLs9qg90lRA39gqPQ79R3NOiteukks_3zpacjvZywy9rLoiR3GsouCpR-ZsXzfwIv7DCBtbHmmAfDkuXLOGt05xuzB72CcAEFkhtCXG9XLCPVIL2nMyJexjLVdD50ETBTE1CFjtcRcARpXNGpXa7dG8PthpB9DC6K-k9Ph94qBHwAEPWTkqQiM=w1157-h866-no?authuser=0"
-						alt=""
-					/>
+					<img src="assets/left1.jpg" alt="" />
 				</div>
 				<div className="imgContainer">
-					<img
-						src="https://lh3.googleusercontent.com/wd4gDrqdjD4nWC7D2tHEN1ORqt_YXc7luGQpOazUMD59vnR79PA9Ygaj6lr7OjgzcLYEjKz4gYSbq18M-fmq00l0cj11Bbk1cfa1_rkSkXLY1GR81sZbNuSlhfjyl1FHuz7jMKNbRMQb-wdvhjbSy_BCAXGyFi30SXPJ07HnovW6P8x4McmGM-DrjYbDb0Ut_4W3yG-f9owhKIw1SjVEidhJhmISxKe08q-cyJASgI1VN5WlvdSFpJ1OvuLwcQoyQu0ngUlca4_45hDPzw4BoITtCOZUAv0paEl-7uxJOIeJSPUDfJ0RTbs6CHqiIOtmzmavsfxrH_-Tlddqt7ZsPHpp5PGyJf7U5oTpc8O3PmliwOxxr5csIZGq8OMb4t3iLFRabTp5mUheaEvirUcpRFlSElA1bLHSABdI04zjdK33mziwg9VRE1hUaCwICF-wQayasK64D927P4pk0RvY7AbH3ygALko1USwcub3KaN24pLUR1ivxS46ihNZTslOLCk5q18Mn6wMW_dnsC5OzMJ6Iq4FK5943w2tNuYdWJvefCTTxolgaZL0ZPQwCC0ULHIm0UInfwGAzkD9RnPkJXZCCaZaHbd_owaVvsBOtcL1VIgQW5B9YsCLLOiFNlHH_wXr9q1uEr7FqqF5LYFOy1cbTQEMFxj9R_Xm5FnClpvSfgfo_DHFQTT5nnpESSLPtGqWkWYC-Sod5koJ3aNz7YffJ=w1154-h866-no?authuser=0"
-						alt=""
-					/>
+					<img src="assets/left2.jpg" alt="" />
 				</div>
 				<div className="imgContainer">
-					<img
-						src="https://lh3.googleusercontent.com/Tl0ndjrJKzIE2GUI34E815t9kt62Vu3aMowxss8L14OJ0P23OQZLTYfTyuuQgpXWnMI5Ewx7HDjkEfYpyAEuGvRHUFylD2kwPOC_FcC4GVkawi2mqv5et-a1PfM5b3XnlKk_QDXbrpk_hUlHZ_JQIjieF9dPNHQ5led63oMRFfLtQHEmKr86GoHydMAbgsisjY0N4ab_iQCBkJxMBLNKyZXPFlUmtny5rE_SHnhj8qthOO7ujgOz5cgPJpD7X12I8Nz-s9lhfOx30YF9Ir7lrgnPdFQGxKG5RduWBuaPmf7UwP9O7KqFeLw_GbXF5OCrhiJTh8WpEVovd7COoyWvew83rWZPi8K5MBy30pzNiWk4wj-ZMVIRIe2CJ91xzPMbsl37R1PeHYYwGCRj5cWMWENMOiUXjDycIXVuxYHKx5HY3m_OFAUycXP_IDO7HfKK4UOZLrKPiaExqk7HwdyPicfN_xNNOhttVDAib72r0XHH1_-pqdldKOTbopOh-x4MIVvVM6_aZtQJ6yz4M0V6_tdQcJl2dUaCw2b3HMKw2SLqaJ8dMq8FdJZ67_3GS6VB2rbp5o-uPK3VCFuSK4X1tVmU6BUY7cCqzDnK20-cebO6xuK1tOFmejAZgrNQe0HxYSk1d7k8CnXo_8WYqBjMnSLSA8oT_EaRannrQ_X0v5aoSKto7gzo5g0L0MoeeorMlMimwIwr4DnvO02xlNln8cVH=w1040-h780-no?authuser=0"
-						alt=""
-					/>
+					<img src="assets/center.jpg" alt="" />
 				</div>
 				<div className="imgContainer">
-					<img
-						src="https://lh3.googleusercontent.com/DsGeiBYTmnFflBTy0RS6bE7dRaEWzh997ivdeMCQw0r-e7t--YHCakEO2VB4oMDHOwZOjFHQtefut6OK-VzMJgnhPXu-3pjW7FszlOF2Ev-3GlNrCjn1c31oLTXH7POTVghxz4QC2wPDgoUdaxr2_bzLO08eDsP3XH1n5kCYYFnycVoZHiYROM16ty8a-uWQvQvAPTfnlHz0vWoGV5EiezvrZAH25tP1lhJR7w3Vw6SIa532oBtG-fDhZ1OauGuB2vJmo16JaCQefxgGN47lIciuQxHuKhpdJ4Cyyy3mlUyn6EowAmrzo4FATpeBrqxQRr0DelgWYueKAYjyXVckG1KZiPiXAYJlCQYfAjkC-HP8pAMCz_qQmU1mbV_Y-mvlt4uRtyL0bQvFQWGMTOQE1O7d7EvqQ-0J1XhyuEUkmNgrUfuDYLTN7L0EYmL9BMMAUwdrrD_TL-jfiP1pxB3bcTG7jxDRi1lJbZ5XkVKfhdTdITF0d_lfV-cDoGPSviRqhpTeiDDL5dmC6PWgESpSWskzu6oaUMmGN-SKuNTjdmwN-tv8GWFTrj0-RfNQETKCvpjl8jlGo3IHJbll9w4KJdCNedyvVLqrhbkC4ehqVKDj4lTwTiASXmA4jQAIK7RJxLLAisFpsobvVLmfxmCHzXOtYA7ghT6ILLAq6B6NwYWDIO7F843I_QcxbT8ThRA8Wg-vSRhdVrEIxcSGd13mWMPR=w1155-h866-no?authuser=0"
-						alt=""
-					/>
+					<img src="right2/portrait.jpg" alt="" />
 				</div>
 				<div className="imgContainer">
-					<img
-						src="https://lh3.googleusercontent.com/-aw1g0VENNhw9b62P1-vMIQnLeXxqHICPvV9qADBgWDUNH6eEaHwzfv7QfQKEPiu6Sca3S_MnkPpRF9micWg1PDHJ6U8QvBCv2UXKb-1-j0YABIzOvXwPTpGfLRulIjXTeBC8kjyctfynICQqekC032qDNE6l01rpDRBcWwjFUTrLdotvtWsVxnZc_TqIhJTTgW0ihRd8BWp0PSInSj5-QH8dbV2xWAwIfq68RiQ0fasPqoX23Rpgr-6x3ig7JGcMrBaZWIXC4_5IiKAEIJ9cJttEncsFCWNMm9dKA-605XR66B6M0QlIVClY9lyMWdg7Zuy4WGxW1jCpteWw6T0PQ3BWKGeQmH_g-iFR4EF00WsCJ9DP4WdJtqJv4mZ_n9J2elLuxSk8T-O6cUMHFDzp7JFTTAe18NoDnZ46UowwZlkZ7ar9QrkCdUHQ1rFeahSNyu3W0p0c-dpk5sXCimloXWAKJwYYLqtK6K3Y7Mpq261tL4zfSiaTlw8YBokLhyJROBOJRBpQTI9tKTaEfxs0-1UtpfkjeQfkkXAialYCZyLLOtzeryKjY600rFG_yNREeBqnHIrMD_hUJ7vewFuX1lJ9ZYVCaFYIE1u2DV6Q4rpKOod4CXM-PHGVG7TnyD29hoK-HQ2e10LDbzV6pdE6NAbWTOik3ylWWf9JQMSKV6lbJbml0HQ1eQgj5APROh9CycGefY3gI4Y4V18tjw8Z6rj=w1154-h866-no?authuser=0"
-						alt=""
-					/>
+					<img src="assets/right1.jpg" alt="" />
 				</div>
 			</div>
 		</div>
